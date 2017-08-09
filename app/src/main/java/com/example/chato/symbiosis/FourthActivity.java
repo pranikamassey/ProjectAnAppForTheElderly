0b03df439d34ad3a6390cc83657606d0a54b3b1a
pranikamassey <pranikamassey@gmail.com>
2017-08-10 00:35:50 +0530

    committ

--- app/src/main/java/com/example/chato/symbiosis/FourthActivity.java
+++ app/src/main/java/com/example/chato/symbiosis/FourthActivity.java
@@ -18,7 +18,9 @@ public class FourthActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_fourth);
+
+
 
 
         final FrameLayout frameLayout = (FrameLayout) findViewById(R.id.containerDetail);
